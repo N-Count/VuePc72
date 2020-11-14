9c8a05612ae10e0d1ea9e54c1bb8f9c1ca5a254d
willson <13580564431@163.com>
2020-11-14 21:54:50 +0800

    完成图片素材上传

--- src/Api/pictures.js
+++ src/Api/pictures.js
@@ -1,9 +1,25 @@
 import axios from '@/utils/request'
+// 获取素材列表
 
-export function reqPictures (params = {}) {
+export const getPictureList = (params) => {
   return axios({
     method: 'GET',
     url: '/mp/v1_0/user/images',
     params
+  })
+}
+export const updatePicture = (id, collect) => {
+  return axios({
+    method: 'PUT',
+    url: `/mp/v1_0/user/images/${id}`,
+    data: {
+      collect
+    }
+  })
+}
+export const delPicture = (id) => {
+  return axios({
+    method: 'DELETE',
+    url: `/mp/v1_0/user/images/${id}`
   })
 }
--- src/views/layout/Pictures/index.vue
+++ src/views/layout/Pictures/index.vue
@@ -5,22 +5,22 @@
               <Hm-breadcrumb>素材管理</Hm-breadcrumb>
         </template>
         <div>
-          <el-radio-group v-model="radio">
-            <el-radio-button label="1">全部</el-radio-button>
-            <el-radio-button label="2">收藏</el-radio-button>
+          <el-radio-group v-model="collect" @change="change">
+            <el-radio-button :label="false">全部</el-radio-button>
+            <el-radio-button :label="true">收藏</el-radio-button>
           </el-radio-group>
-          <el-button type="success" style="float:right;">上传素材</el-button>
+          <el-button type="success" style="float:right;" @click="dialogVisible = true">上传素材</el-button>
         </div>
-        <el-row :gutter="10">
-            <el-col :span="4" v-for="item in 12" :key="item.id">
+        <el-row :gutter="10" v-loading="loading">
+            <el-col :span="4" v-for="item in pictures" :key="item.id">
            <el-image
             style="height: 180px"
-            src="https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg"
+            :src="item.url"
             fit="cover">
             </el-image>
-            <div class="option">
-              <span class="el-icon-star-off" :class="{collect: item.is_collected}" @click="update(item)"></span>
-              <span class="el-icon-delete"></span>
+            <div class="option" v-if="!collect">
+              <span class="el-icon-star-off" :class="{active: item.is_collected}" @click="update(item)"></span>
+              <span class="el-icon-delete" @click='del(item.id)'></span>
             </div>
            </el-col>
         </el-row>
@@ -33,19 +33,53 @@
         :total="total">
       </el-pagination>
       </el-card>
+<!-- 上传框 -->
+       <el-dialog
+      title="上传素材"
+      :visible.sync="dialogVisible"
+      width="30%">
+      <!--
+        el-upload 上传组件
+        action： 上传图片的地址  注意：必须要提供完整的地址，，，因为element上传图片不走axios
+        headers: 因为我们的接口上传图片需要传token，，但是element不走axios
+        name="image" 指定上传的图片的名字
+        show-file-list: 是否显示上传的文件列表
+        on-success： 上传成功的回调
+        before-upload： 选完图片，上传图片之前的回调
+       -->
+       <el-upload
+        class="avatar-uploader"
+        action="http://ttapi.research.itcast.cn/mp/v1_0/user/images"
+        :headers="headers"
+        name="image"
+        :show-file-list="false"
+        :on-success="onSuccess"
+        :before-upload="beforeUpload"
+      >
+        <img class="avatar" v-if="imgUrl" :src="imgUrl" alt="">
+        <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+      </el-upload>
+       </el-dialog>
     </div>
 </template>
 <script>
-import { reqPictures } from '@/Api/pictures'
+import { getPictureList, updatePicture, delPicture } from '@/Api/pictures'
+import { getToken } from '@/utils/storage'
 export default {
   name: 'Pictrues',
   data () {
     return {
-      radio: '1',
+      collect: false,
       pictures: [],
       page: 1,
       per_page: 12,
-      total: 0
+      total: 0,
+      loading: false,
+      dialogVisible: false,
+      imgUrl: '',
+      headers: {
+        Authorization: 'Bearer ' + getToken()
+      }
     }
   },
   created () {
@@ -53,17 +87,77 @@ export default {
   },
   methods: {
     async getPictures () {
-      const res = await reqPictures({
-        page: 1,
-        per_page: 12
+      this.loading = true
+      const res = await getPictureList({
+        page: this.page,
+        per_page: this.per_page,
+        collect: this.collect
       })
       this.pictures = res.data.data.results
+      this.total = res.data.data.total_count
+      this.loading = false
     },
-    update () {
-      console.log('变色')
+    handleCurrentChange (current) {
+      this.page = current
+      this.getPictures()
+    },
+    change () {
+      this.page = 1
+      this.getPictures()
+    },
+    async update (item) {
+      const res = await updatePicture(item.id, !item.is_collected)
+      console.log(res)
+      // 取消收藏后发送请求重新获取图片数据
+      // this.getPictures()
+      // 不取消收藏，直接通过取反来使收藏按钮变回白色
+      item.is_collected = !item.is_collected
+      this.$message.success(item.is_collected ? '取消收藏成功' : '收藏成功')
+    },
+    async del (id) {
+      try {
+        await this.$confirm('确定删除该图片吗？', {
+          type: 'warning'
+        })
+      } catch {
+        return
+      }
+      try {
+        await delPicture(id)
+        this.$message.success('删除成功！')
+        if (this.pictures.length === 1) {
+          this.page--
+        }
+        this.getPictures()
+      } catch {
+        this.$message.error('删除失败，请稍后重试')
+      }
+    },
+    onSuccess (res) {
+      this.imgUrl = res.data.url
+      this.$message.success('上传素材成功')
+      setTimeout(() => {
+        this.dialogVisible = false
+        this.imgUrl = ''
+        this.getPictures()
+      }, 2000)
+    },
+    beforeUpload (file) {
+      // 要求：只能上传png 或者 jpg 或者 gif 其他文件不让上传
+      // 要求2： 只能上传2m以内的图片
+      console.log(file)
+      if (!['image/png', 'image/jpeg', 'image/gif'].includes(file.type)) {
+        this.$message.error('上传的图片格式不正确')
+        return false
+      }
+      if (file.size / 1024 / 1024 >= 2) {
+        this.$message.error('上传文件超过了2M')
+        return false
+      }
     }
   }
 }
+
 </script>
 <style scoped lang="less">
 .pictures {
